7de6858641bbb3289b133ebff4275ecb86ce03d1
Synnysyn <69205616+Synnysyn@users.noreply.github.com>
2021-05-07 19:01:53 +0200

    small update

--- bookshelf_app/models.py
+++ bookshelf_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    """
+    Book model, to be finished
+    """
+
+    title = models.CharField(max_length=64)
+    author = models.CharField(max_length=64)
+    added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'"{self.title}" by {self.author}'
